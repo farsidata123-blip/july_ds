269d2cef495786ee0783a511c02bdc8911eed9ff
farseena <farseena638@gmail.com>
2025-12-10 13:30:24 +0530

    msg1

---
+++ work2.py
@@ -0,0 +1,20 @@
+def add(x,y):
+    return x+y
+
+def add(x,y):
+    return x+y
+
+def add(x,y):
+    return x+y
+
+def add(x,y):
+    return x+y
+
+def add(x,y):
+    return x+y
+
+def add(x,y):
+    return x+y
+
+def add(x,y):
+    return x+y
